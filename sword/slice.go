ece0bedae35658dcbccda94de9174fe0c8014cad
MenciusCheng <295567172@qq.com>
2022-02-09 10:35:20 +0800

    slice

---
+++ sword/slice.go
@@ -0,0 +1,180 @@
+package sword
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func arrayExample() {
+	// 数组主要有三种声明方式
+	var arr [3]int
+	var arr2 = [4]int{1, 2, 3, 4}
+	arr3 := [...]int{2, 3, 4} // 语法糖，依靠编译器进行数组长度的推断。
+
+	fmt.Printf("arr: %+v, len: %d\n", arr, len(arr))
+	fmt.Printf("arr2: %+v, len: %d\n", arr2, len(arr2))
+	fmt.Printf("arr3: %+v, len: %d\n", arr3, len(arr3))
+
+	// 数组值复制
+	arr4 := arr2
+	arr4[0] = 4
+	fmt.Printf("arr4: %+v, arr2: %+v\n", arr4, arr2)
+}
+
+func sliceExample() {
+	// 声明切片
+	var slice1 []int
+	slice2 := make([]int, 5)
+	slice3 := make([]int, 0, 5)
+	numbers := []int{1, 2, 3, 4, 5}
+
+	fmt.Printf("slice1: %+v, len: %d, cap: %d\n", slice1, len(slice1), cap(slice1))
+	fmt.Printf("slice2: %+v, len: %d, cap: %d\n", slice2, len(slice2), cap(slice2))
+	fmt.Printf("slice3: %+v, len: %d, cap: %d\n", slice3, len(slice3), cap(slice3))
+	fmt.Printf("numbers: %+v, len: %d, cap: %d\n", numbers, len(numbers), cap(numbers))
+
+	// 切片的截取
+	num1 := numbers[1:3] // 截取范围：[1,3)
+	num2 := numbers[:2]  // [0,2)
+	num3 := numbers[2:]  // [2,len(num3))
+	fmt.Printf("num1: %+v, len: %d, cap: %d\n", num1, len(num1), cap(num1))
+	fmt.Printf("num2: %+v, len: %d, cap: %d\n", num2, len(num2), cap(num2))
+	fmt.Printf("num3: %+v, len: %d, cap: %d\n", num3, len(num3), cap(num3))
+
+	// 切片的复制，是底层指针的值复制，可以理解为数据的引用传递
+	num4 := numbers
+	num4[0] = 4
+	fmt.Printf("num4: %+v, numbers: %+v\n", num4, numbers)
+
+	// 切片添加元素
+	num5 := make([]int, 0)
+	num5 = append(num5, 0)
+	num5 = append(num5, 1, 2, 3)
+	num5 = append(num5, []int{4, 5, 6}...)
+	fmt.Printf("num5: %+v\n", num5)
+
+	// 切片删除元素
+	num5 = num5[1:] // 删除第一个元素
+	fmt.Printf("num5: %+v\n", num5)
+	num5 = num5[:len(num5)-1] // 删除最后一个元素
+	fmt.Printf("num5: %+v\n", num5)
+	num5 = append(num5[:2], num5[3:]...) // 删除下标为2的元素
+	fmt.Printf("num5: %+v\n", num5)
+
+	// 切片的深拷贝，新切片的修改不会影响旧切片
+	num6 := []int{1, 2, 3}
+	num7 := make([]int, len(num6), len(num6))
+	copy(num7, num6)
+	num7[0] = 4
+	fmt.Printf("num6: %+v, len: %d, cap: %d\n", num6, len(num6), cap(num6))
+	fmt.Printf("num7: %+v, len: %d, cap: %d\n", num7, len(num7), cap(num7))
+}
+
+/*
+[167. 两数之和 II - 输入有序数组](https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/)
+
+题目大意：输入一个递增排序的数组和一个值k，请问如何在数组中找出两个和为k的数字并返回它们的下标？
+假设数组中存在且只存在一对符合条件的数字，同时一个数字不能使用两次。下标从1开始。
+例如，输入数组[1，2，4，6，10]，k的值为8，数组中的数字2与6的和为8，由于下标从1开始，则它们的下标分别为2与4。
+*/
+func twoSum(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+	for i < j {
+		sum := numbers[i] + numbers[j]
+		if sum == target {
+			return []int{i + 1, j + 1}
+		} else if sum > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return []int{}
+}
+
+/*
+[15. 三数之和](https://leetcode-cn.com/problems/3sum/)
+
+题目大意：输入一个数组，如何找出数组中所有和为0的3个数字的三元组？需要注意的是，返回值中不得包含重复的三元组。
+例如，在数组[-1，0，1，2，-1，-4]中有两个三元组的和为0，它们分别是[-1，0，1]和[-1，-1，2]。
+*/
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := make([][]int, 0)
+	last := len(nums) - 1
+	for i := 0; i < len(nums)-2; i++ {
+		j, k := i+1, last
+
+		for j < k {
+			sum := nums[i] + nums[j] + nums[k]
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[j], nums[k]})
+
+				for j+1 < len(nums) && nums[j] == nums[j+1] {
+					j++
+				}
+				j++
+			} else if sum > 0 {
+				k--
+			} else {
+				j++
+			}
+		}
+
+		for i+1 < len(nums) && nums[i] == nums[i+1] {
+			i++
+		}
+	}
+	return res
+}
+
+/*
+[209. 长度最小的子数组](https://leetcode-cn.com/problems/minimum-size-subarray-sum/)
+
+题目大意：输入一个正整数组成的数组和一个正整数k，请问数组中和大于或等于k的连续子数组的最短长度是多少？如果不存在所有数字之和大于或等于k的子数组，则返回0。
+例如，输入数组[5，1，4，3]，k的值为7，和大于或等于7的最短连续子数组是[4，3]，因此输出它的长度2。
+*/
+func minSubArrayLen(target int, nums []int) int {
+	sum := 0
+	res := math.MaxInt
+	li := 0
+	for ri := 0; ri < len(nums); ri++ {
+		sum += nums[ri]
+		for sum >= target {
+			res = min(res, ri-li+1)
+			if res == 1 {
+				return res
+			}
+			sum -= nums[li]
+			li++
+		}
+	}
+
+	if res == math.MaxInt {
+		return 0
+	}
+	return res
+}
+
+/*
+[713. 乘积小于K的子数组](https://leetcode-cn.com/problems/subarray-product-less-than-k/)
+
+题目大意：输入一个由正整数组成的数组和一个正整数k，请问数组中有多少个数字乘积小于k的连续子数组？
+例如，输入数组[10，5，2，6]，k的值为100，有8个子数组的所有数字的乘积小于100，它们分别是[10]、[5]、[2]、[6]、[10，5]、[5，2]、[2，6]和[5，2，6]。
+*/
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	res := 0
+	product := 1
+
+	li := 0
+	for ri := 0; ri < len(nums); ri++ {
+		product *= nums[ri]
+		for product >= k && li <= ri {
+			product /= nums[li]
+			li++
+		}
+		res += ri - li + 1
+	}
+	return res
+}
---
+++ sword/slice_test.go
@@ -0,0 +1,176 @@
+package sword
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_arrayExample(t *testing.T) {
+	arrayExample()
+}
+
+func Test_sliceExample(t *testing.T) {
+	sliceExample()
+}
+
+func Test_twoSum(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			args: args{
+				numbers: []int{2, 7, 11, 15},
+				target:  9,
+			},
+			want: []int{1, 2},
+		},
+		{
+			args: args{
+				numbers: []int{2, 3, 4},
+				target:  6,
+			},
+			want: []int{1, 3},
+		},
+		{
+			args: args{
+				numbers: []int{-1, 0},
+				target:  -1,
+			},
+			want: []int{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_threeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			args: args{
+				nums: []int{-1, 0, 1, 2, -1, -4},
+			},
+			want: [][]int{
+				{-1, -1, 2}, {-1, 0, 1},
+			},
+		},
+		{
+			args: args{
+				nums: []int{},
+			},
+			want: [][]int{},
+		},
+		{
+			args: args{
+				nums: []int{0},
+			},
+			want: [][]int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_minSubArrayLen(t *testing.T) {
+	type args struct {
+		target int
+		nums   []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			args: args{
+				target: 7,
+				nums:   []int{2, 3, 1, 2, 4, 3},
+			},
+			want: 2,
+		},
+		{
+			args: args{
+				target: 4,
+				nums:   []int{1, 4, 4},
+			},
+			want: 1,
+		},
+		{
+			args: args{
+				target: 11,
+				nums:   []int{1, 1, 1, 1, 1, 1, 1, 1},
+			},
+			want: 0,
+		},
+		{
+			args: args{
+				target: 15,
+				nums:   []int{1, 2, 3, 4, 5},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minSubArrayLen(tt.args.target, tt.args.nums); got != tt.want {
+				t.Errorf("minSubArrayLen() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_numSubarrayProductLessThanK(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			args: args{
+				nums: []int{10, 5, 2, 6},
+				k:    100,
+			},
+			want: 8,
+		},
+		{
+			args: args{
+				nums: []int{1, 2, 3},
+				k:    0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numSubarrayProductLessThanK(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("numSubarrayProductLessThanK() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
